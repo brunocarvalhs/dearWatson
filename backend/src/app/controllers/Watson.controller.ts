5bda4c0e618bfce9276b9b24c3f559b37fc9a74c
brunocarvalhs <bruno.silva.carvalho@outlook.com.br>
2020-08-11 11:45:56 -0300

    📘  Implementation of singleton design patterns

--- backend/src/app/controllers/Watson.controller.ts
+++ backend/src/app/controllers/Watson.controller.ts
@@ -1,83 +1,99 @@
 import {
   ToneAnalyzerV3,
-  NaturalLanguageClassifierV1,
-  VisualRecognitionV3,
-  PersonalityInsightsV3,
+  NaturalLanguageUnderstandingV1,
+  PersonalityInsightsV3
 } from "ibm-watson/sdk";
+import VisualRecognitionV4 from "ibm-watson/visual-recognition/v4";
 import { IamAuthenticator } from "ibm-watson/auth";
-import { ToneAnalyzerConfig } from "../../config/watson.config";
+import {
+  ToneAnalyzerConfig as TAConfig,
+  NaturalLanguageUnderstandingConfig as NLUConfig,
+  PersonalityInsightsConfig as PIConfig,
+  VisualRecognitionConfig as VRConfig,
+} from "../../config/watson.config";
 
 class Watson {
+  // Instance
+  private static ToneAnalyzer: ToneAnalyzerV3;
+  private static NaturalLanguageUnderstanding: NaturalLanguageUnderstandingV1;
+  private static PersonalityInsights: PersonalityInsightsV3;
+  private static VisualRecognition: VisualRecognitionV4;
 
-    // Instance
-    private static ToneAnalyzer: ToneAnalyzerV3;
-    private static NaturalLanguageClassifier: NaturalLanguageClassifierV1;
-    private static PersonalityInsights: PersonalityInsightsV3;
-    private static VisualRecognition: VisualRecognitionV3;
-
-    /**
-     * Method responsible for checking the 
-     * existing instance of ToneAnalyzerV3
-     * 
-     * @returns ToneAnalyzerV3: ToneAnalyzerV3
-     */
-    protected getInstanceToneAnaluzer(): ToneAnalyzerV3 {
-        if (!Watson.ToneAnalyzer) {
-        Watson.ToneAnalyzer = new ToneAnalyzerV3({
-            version: ToneAnalyzerConfig.version,
-            authenticator: new IamAuthenticator({
-            apikey: ToneAnalyzerConfig.apiKey,
-            }),
-            url: ToneAnalyzerConfig.URL,
-        });
-        }
-        return Watson.ToneAnalyzer;
+  /**
+   * Method responsible for checking the
+   * existing instance of ToneAnalyzerV3
+   *
+   * @returns ToneAnalyzerV3: ToneAnalyzerV3
+   */
+  public static getInstanceToneAnaluzer(): ToneAnalyzerV3 {
+    if (!Watson.ToneAnalyzer) {
+      Watson.ToneAnalyzer = new ToneAnalyzerV3({
+        version: TAConfig.version,
+        authenticator: new IamAuthenticator({
+          apikey: TAConfig.apiKey,
+        }),
+        url: TAConfig.URL,
+      });
     }
+    return Watson.ToneAnalyzer;
+  }
 
-    /**
-     * Method responsible for checking the 
-     * existing instance of NaturalLanguageClassifierV1
-     * 
-     * @returns NaturalLanguageClassifierV1: NaturalLanguageClassifierV1
-     */
-    protected getInstanceNaturalLanguage(): NaturalLanguageClassifierV1 {
-        if(!Watson.NaturalLanguageClassifier){
-            Watson.NaturalLanguageClassifier = new NaturalLanguageClassifierV1({
-
-            });
-        }
-        return Watson.NaturalLanguageClassifier;
+  /**
+   * Method responsible for checking the
+   * existing instance of NaturalLanguageUnderstandingV1
+   *
+   * @returns NaturalLanguageUnderstandingV1: NaturalLanguageUnderstandingV1
+   */
+  public static getInstanceNaturalLanguage(): NaturalLanguageUnderstandingV1 {
+    if (!Watson.NaturalLanguageUnderstanding) {
+      Watson.NaturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: NLUConfig.version,
+        authenticator: new IamAuthenticator({
+          apikey: NLUConfig.apiKey,
+        }),
+        url: NLUConfig.URL,
+      });
     }
+    return Watson.NaturalLanguageUnderstanding;
+  }
 
-    /**
-     * Method responsible for checking the 
-     * existing instance of PersonalityInsightsV3
-     * 
-     * @returns PersonalityInsightsV3: PersonalityInsightsV3
-     */
-    protected getInstancePersonalityInsights(): PersonalityInsightsV3 {
-        if(!Watson.PersonalityInsights){
-            Watson.PersonalityInsights = new PersonalityInsightsV3({
-
-            });
-        }
-        return Watson.PersonalityInsights;
+  /**
+   * Method responsible for checking the
+   * existing instance of PersonalityInsightsV3
+   *
+   * @returns PersonalityInsightsV3: PersonalityInsightsV3
+   */
+  public static getInstancePersonalityInsights(): PersonalityInsightsV3 {
+    if (!Watson.PersonalityInsights) {
+      Watson.PersonalityInsights = new PersonalityInsightsV3({
+        version: PIConfig.version,
+        authenticator: new IamAuthenticator({
+          apikey: PIConfig.apiKey,
+        }),
+        url: PIConfig.URL,
+      });
     }
+    return Watson.PersonalityInsights;
+  }
 
-    /**
-     * Method responsible for checking the 
-     * existing instance of VisualRecognitionV3
-     * 
-     * @returns VisualRecognitionV3: VisualRecognitionV3
-     */
-    protected getInstancPersonalityInsights(): VisualRecognitionV3 {
-        if(!Watson.VisualRecognition){
-            Watson.VisualRecognition = new VisualRecognitionV3({
-
-            });
-        }
-        return Watson.VisualRecognition;
+  /**
+   * Method responsible for checking the
+   * existing instance of VisualRecognitionV4
+   *
+   * @returns VisualRecognitionV4: VisualRecognitionV4
+   */
+  public static getInstancVisualRecognition(): VisualRecognitionV4 {
+    if (!Watson.VisualRecognition) {
+      Watson.VisualRecognition = new VisualRecognitionV4({
+        version: VRConfig.version,
+        authenticator: new IamAuthenticator({
+          apikey: VRConfig.apiKey,
+        }),
+        url: VRConfig.URL,
+      });
     }
+    return Watson.VisualRecognition;
+  }
 }
 
 export default Watson;

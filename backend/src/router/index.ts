fded9e0f85626bd5847b4b1e52bed00e2d02af75
brunocarvalhs <bruno.silva.carvalho@outlook.com.br>
2020-08-11 11:47:26 -0300

    💻  Encoding service request

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import ToneAnalyzerController from '../app/controllers/ToneAnalyzer.controller';
+import NaturalLanguageController from '../app/controllers/NaturalLanguage.controller';
 
 const router = Router();
 
@@ -11,5 +12,11 @@ router.get('/tone/v1/index',ToneAnalyzerController.index);
 router.post('/tone/v1/create',ToneAnalyzerController.create);
 router.put('/tone/v1/update',ToneAnalyzerController.update);
 router.delete('/tone/v1/delete',ToneAnalyzerController.delete);
+
+router.get('/natural/v1/index',NaturalLanguageController.index);
+router.post('/natural/v1/create',NaturalLanguageController.create);
+router.put('/natural/v1/update',NaturalLanguageController.update);
+router.delete('/natural/v1/delete',NaturalLanguageController.delete);
+
 
 export default router;

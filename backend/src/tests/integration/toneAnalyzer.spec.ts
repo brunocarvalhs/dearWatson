06aec3883ab2bf95f93c12cd602daa94fe551278
brunocarvalhs <bruno.silva.carvalho@outlook.com.br>
2020-08-11 11:42:28 -0300

    🔍 Testing implementation

--- backend/src/tests/integration/toneAnalyzer.spec.ts
+++ backend/src/tests/integration/toneAnalyzer.spec.ts
@@ -2,15 +2,12 @@ import request from 'supertest';
 import app from '../../app';
 
 describe('TONE ANALYZER', () => {
-    it('must be able to analyze the text and returns the emotions present in the text.', async () => {
+    test('must be able to analyze the text and returns the emotions present in the text.', async () => {
         const response = await request(app)
         .post('/tone/v1/create')
         .send({
-            text: ` Team, I know that times are tough! Product sales have been disappointing 
-                    for the past three quarters. We have a competitive product, but we need 
-                    to do a better job of selling it!`
+            text: "Team, I know that times are tough! Product sales have been disappointing  for the past three quarters. We have a competitive product, but we need to do a better job of selling it!"
         });
-
-        console.log(response.body);
+        expect(response.status).toBe(200);
     });
 });

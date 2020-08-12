47788ad750ab3f4e41e7cd95c9afe6497e2982a2
brunocarvalhs <bruno.silva.carvalho@outlook.com.br>
2020-08-12 12:11:27 -0300

    🛣  Generating new API routes

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,22 +1,37 @@
 import { Router } from "express";
-import ToneAnalyzerController from '../app/controllers/ToneAnalyzer.controller';
-import NaturalLanguageController from '../app/controllers/NaturalLanguage.controller';
+
+//Validator
+import {
+  AnalyzeGeneralTone,
+  AnalyzeCustomerEngagementTone,
+} from "../app/validator/ToneAnalyzer.validator";
+import { AnalyzeText } from "../app/validator/NatualLanguage.validator";
+
+// Controllers
+import ToneAnalyzerController from "../app/controllers/ToneAnalyzer.controller";
+import NaturalLanguageController from "../app/controllers/NaturalLanguage.controller";
 
 const router = Router();
 
-router.get('/', (request, response) =>{
-    return response.status(200).json({message: "Olá"});
+router.get("/", (request, response) => {
+  return response.status(200).json({ message: "Olá" });
 });
 
-router.get('/tone/v1/index',ToneAnalyzerController.index);
-router.post('/tone/v1/create',ToneAnalyzerController.create);
-router.put('/tone/v1/update',ToneAnalyzerController.update);
-router.delete('/tone/v1/delete',ToneAnalyzerController.delete);
+router.post(
+  "/tone/v1/analyze",
+  AnalyzeGeneralTone,
+  ToneAnalyzerController.AnalyzeGeneralTone
+);
+router.post(
+  "/tone/v1/chat",
+  AnalyzeCustomerEngagementTone,
+  ToneAnalyzerController.AnalyzeCustomerEngagementTone
+);
 
-router.get('/natural/v1/index',NaturalLanguageController.index);
-router.post('/natural/v1/create',NaturalLanguageController.create);
-router.put('/natural/v1/update',NaturalLanguageController.update);
-router.delete('/natural/v1/delete',NaturalLanguageController.delete);
-
+router.post(
+  "/natural/v1/create",
+  AnalyzeText,
+  NaturalLanguageController.AnalyzeText
+);
 
 export default router;
